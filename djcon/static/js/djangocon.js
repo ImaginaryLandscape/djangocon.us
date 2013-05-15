60e3671a0d2412876732afe93f14b055aba45e98
Dave Bertrand <dbertrand@imagescape.com>
2013-05-15 20:22:11 +0000

    IE custom styles and switching nav to a site tree object

--- djcon/static/js/djangocon.js
+++ djcon/static/js/djangocon.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+
+    if (typeof console === "undefined"){
+        console={};
+        console.log = function(){
+            return;
+        }
+    }
+
     $("#logout_link").on("click", function(e) {
         var form = $($(this).data("form-submit"));
         form.submit();
