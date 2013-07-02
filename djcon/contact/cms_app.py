df7d0be228471d14efd4ce72012147417a796b4a
Dave Bertrand <dbertrand@imagescape.com>
2013-07-02 21:24:00 +0000

    removed unneccesary file

--- djcon/contact/cms_app.py
+++
@@ -1,9 +0,0 @@
-from cms.app_base import CMSApp
-from cms.apphook_pool import apphook_pool
-from django.utils.translation import ugettext_lazy as _
-
-class ContactApp(CMSApp):
-    name = _("Iscape Contact Form") # give your app a name, this is required
-    urls = ["djcon.contact.urls"] # link your app to url configuration(s)
-
-apphook_pool.register(ContactApp) # register your app
